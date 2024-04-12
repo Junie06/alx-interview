8d81dd9e793aa162b74e77138d8916bd18b8aa01
Isoken Ibizugbe <isokenjune@gmail.com>
2024-04-12 02:18:01 +0000

    pascal triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+''' returns a list of lists of integers representing the Pascal triangle of n'''
+
+def pascal_triangle(n):
+        triangle = []
+        for i in range(n):
+            row = []
+            for j in range(i+1):
+                if (j == 0) or (j == i):
+                    row.append(1)
+                else:
+                    row.append(triangle[i-1][j-1] + triangle[i-1][j])
+                    triangle.append(row)
+        return triangle
