7c618fe95e2f5df46125588d61f35cfb53694860
Isoken June Ibizugbe <isokenjune@gmail.com>
2024-04-14 14:33:05 +0100

    corrected errors

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
-''' returns a list of lists of integers representing the Pascal triangle of n'''
+'''returns a list of lists of integers representing the Pascal triangle of n'''
 
 
 def pascal_triangle(n):
-	''' pascal traingle function'''
+    ''' pascal traingle function'''
 
-	triangle = []
-	for i in range(n):
-		row = []
-		for j in range(i+1):
-			if (j == 0) or (j == i):
-				row.append(1)
-			else:
-				row.append(triangle[i-1][j-1] + triangle[i-1][j])
-				triangle.append(row)
-	return triangle
+    triangle = []
+    for i in range(n):
+        row = []
+        for j in range(i+1):
+            if (j == 0) or (j == i):
+                row.append(1)
+            else:
+                row.append(triangle[i-1][j-1] + triangle[i-1][j])
+        triangle.append(row)
+    return triangle
